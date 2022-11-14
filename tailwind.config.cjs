203a11bd42eb588e6e066e300d5aec3c36a017eb
DESKTOP-L6JGSEF\cesin <cesinhaobdc@gmail.com>
2022-11-13 21:57:34 -0300

    update dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.jsx"],
   theme: {
     fontFamily: {
       sans: ['Roboto', 'sans-serif'],
